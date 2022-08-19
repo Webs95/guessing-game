215f5358c71d0ca569e56c72be87ffa1e611498d
Webs95 <alexandersemashko1995@yandex.ru>
2022-08-19 22:30:50 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.res = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.res;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.res;
+        this.guess();
     }
 }
 
